0a9e4e13af448dd9fb44590b38341c09281fec96
zach <zacharytq@gmail.com>
2021-04-28 18:21:04 -0400

    working on frontend authentication

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import './App.css';
-import React, { Component } from 'react';
-import UploadFile from './components/UploadFile';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import NormalRoute from "./components/NormalRoute";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Signup from "./components/auth/Signup";
+import Login from "./components/auth/Login";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <UploadFile />
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div className='App'>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={NormalRoute} />
+          <Route exact path='/protected_route' component={ProtectedRoute} />
+          <Route exact path='/signup' component={Signup} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
+
 export default App;
