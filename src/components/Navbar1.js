0fefb47c0203915f96ec5b8eed051a8ad480e51b
zach <zacharytq@gmail.com>
2021-05-11 11:02:50 -0400

    drawer now has list of links in it

--- src/components/Navbar1.js
+++ src/components/Navbar1.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
-import { AppBar, Toolbar, IconButton, Drawer, makeStyles } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Drawer, makeStyles, MenuList, MenuItem, ListItemText } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import Routes from '../Routes'
+import { NavLink, withRouter } from 'react-router-dom';
 
 const useStyles = makeStyles(() => ({
   drawer: {
@@ -8,13 +10,21 @@ const useStyles = makeStyles(() => ({
   },
   drawerPaper: {
     width: 240
+  },
+  list: {
+    width: 'auto'
   }
 }))
 
-const Navbar = () => {
+const Navbar = (props) => {
   const classes = useStyles();
   const [ isOpen, setIsOpen ] = useState(false);
   const toggleIsOpen = () => setIsOpen(!isOpen)
+
+  const activeRoute = (route) => {
+    return route === props.location.pathname ? true : false;
+  }
+
   return (
     <div>
       <div>
@@ -27,9 +37,26 @@ const Navbar = () => {
         </AppBar>
       </div>
       <Drawer className={classes.drawer} open={isOpen} onClose={toggleIsOpen} classes={{ paper: classes.drawerPaper}}>
+        <div
+          className={classes.list}
+          role='presentation'
+          onClick={toggleIsOpen}
+        >
+          <MenuList>
+            {Routes.map((route, key) => {
+              return (
+                <NavLink to={route.path} key={key}>
+                  <MenuItem selected={activeRoute(route.path)}>
+                    <ListItemText primary={route.sidebarName} />
+                  </MenuItem>
+                </NavLink>
+              ) 
+            })}
+          </MenuList>
+        </div>
       </Drawer>
     </div>
   )
 }
 
-export default Navbar;
+export default withRouter(Navbar);
